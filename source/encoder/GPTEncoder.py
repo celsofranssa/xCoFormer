ca2ccbcc7d71534657c135af3fe9ab4d0bc4fcd2
Celso <celsofranca@dcc.ufmg.br>
2021-01-05 22:43:50 -0300

    feat: add SelfAttentionEncoder.

---
+++ source/encoder/GPTEncoder.py
@@ -0,0 +1,26 @@
+from omegaconf import OmegaConf
+from pytorch_lightning import LightningModule
+from transformers import BertModel
+
+from source.model.AveragePooling import AveragePooling
+
+
+class GPTEncoder(LightningModule):
+    """Encodes the input as embeddings."""
+
+    def __init__(self, hparams):
+        super(GPTEncoder, self).__init__()
+        self.bert_encoder = BertModel.from_pretrained(
+            hparams.architecture,
+            output_attentions=hparams.output_attentions
+        )
+        self.pooling = AveragePooling()
+
+    def forward(self, features):
+        attention_mask = (features > 1).int()
+        hidden_states = self.bert_encoder(features, attention_mask)[0]
+
+        return self.pooling(
+            attention_mask,
+            hidden_states
+        )
---
+++ source/encoder/SelfAttentionEncoder.py
@@ -0,0 +1,37 @@
+import torch.nn
+from pytorch_lightning import LightningModule
+from torch import nn
+
+from source.model.AveragePooling import AveragePooling
+
+
+class SelfAttentionEncoder(LightningModule):
+    """Encodes the input as embeddings."""
+
+    def __init__(self, hparams):
+        super(SelfAttentionEncoder, self).__init__()
+
+        self.value_layer = nn.Embedding(
+            num_embeddings=hparams.vocabulary_size,
+            embedding_dim=hparams.representation_size
+        )
+
+        self.query_layer = nn.Linear(hparams.representation_size, hparams.representation_size)
+        self.key_layer = nn.Linear(hparams.representation_size, hparams.representation_size)
+
+        self.multihead_attn = torch.nn.MultiheadAttention(hparams.representation_size, hparams.num_heads,
+                                                          dropout=hparams.dropout)
+
+        self.pool = AveragePooling()
+
+    def forward(self, x):
+        attention_mask = (x > 0).int()
+
+        value = torch.transpose(self.value_layer(x), 0, 1)
+        query = self.query_layer(value)
+        key = self.key_layer(value)
+
+        attn_output, _ = self.multihead_attn(query, key, value)
+        attn_output = torch.transpose(attn_output, 0, 1)
+
+        return self.pool(attention_mask, attn_output)
