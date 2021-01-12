3cf0c0f555c64266cad6bcecfb19544e5f9bc774
Celso <celsofranca@dcc.ufmg.br>
2021-01-12 15:14:29 -0300

    stats: add codeberta config.

--- source/encoder/CodeBERTaEncoder.py
+++ source/encoder/CodeBERTaEncoder.py
@@ -7,15 +7,18 @@ from source.model.AveragePooling import AveragePooling
 class CodeBertaEncoder(LightningModule):
     """Encodes the input as embeddings."""
 
-    def __init__(self):
+    def __init__(self, hparams):
         super(CodeBertaEncoder, self).__init__()
-        self.codeberta_encoder = RobertaModel.from_pretrained("huggingface/CodeBERTa-small-v1")
+        self.codeberta_encoder = RobertaModel.from_pretrained(
+            hparams.architecture,
+            output_attentions=hparams.output_attentions
+        )
         self.pooling = AveragePooling()
 
     def forward(self, features):
-        # print(features)
-        attention_mask = (features > 2).int()
-        hidden_states = self.codeberta_encoder(features, attention_mask)[0]
+
+        attention_mask = (features != 1).int()
+        hidden_states = self.codeberta_encoder(features, attention_mask).last_hidden_state
 
         return self.pooling(
             attention_mask,
