4288c0a3cd8a851cc325f863ac6eec9362a21162
celso <celsofranca@dcc.ufmg.br>
2021-01-20 13:15:52 -0300

    feat: add xSingleFormer.py.

---
+++ source/model/SingleEncoder.py
@@ -0,0 +1,95 @@
+import importlib
+
+import torch
+from pytorch_lightning.core.lightning import LightningModule
+
+from source.metric.MRRMetric import MRRMetric
+
+
+class SingleEncoder(LightningModule):
+    """Encodes the code and desc into an same space of embeddings."""
+
+    def __init__(self, hparams):
+
+        super(SingleEncoder, self).__init__()
+        self.hparams = hparams
+
+        # encoders
+        self.encoder = self.get_encoder(hparams.x1_encoder, hparams.x1_encoder_hparams)
+
+        # loss function
+        self.loss_fn = self.get_loss(hparams.loss, hparams.loss_hparams)
+
+        # metric
+        self.mrr = MRRMetric()
+
+    def get_encoder(self, encoder, encoder_hparams):
+        encoder_module, encoder_class = encoder.rsplit('.', 1)
+        encoder_module = importlib.import_module(encoder_module)
+        return getattr(encoder_module, encoder_class)(encoder_hparams)
+
+    def get_loss(self, loss, loss_hparams):
+        loss_module, loss_class = loss.rsplit('.', 1)
+        loss_module = importlib.import_module(loss_module)
+        return getattr(loss_module, loss_class)(loss_hparams)
+
+    def forward(self, x1, x2):
+        r1 = self.encoder(x1)
+        r2 = self.encoder(x2)
+        return r1, r2
+
+    def configure_optimizers(self):
+        # optimizers
+        optimizer = torch.optim.Adam(self.x1_encoder.parameters(), lr=self.hparams.lr, betas=(0.9, 0.999), eps=1e-08,
+                             weight_decay=0, amsgrad=True)
+
+        # schedulers
+        step_size_up = 0.03 * self.num_training_steps
+
+        scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, mode='triangular2', base_lr=1e-7, max_lr=1e-3,
+                                              step_size_up=step_size_up, cycle_momentum=False)
+        return optimizer, scheduler
+
+
+
+    def training_step(self, batch, batch_idx, optimizer_idx):
+
+        x1, x2 = batch["x1"], batch["x2"]
+        r1, r2 = self(x1, x2)
+        train_loss = self.loss_fn(r1, r2)
+        return train_loss
+
+    def validation_step(self, batch, batch_idx):
+        x1, x2 = batch["x1"], batch["x2"]
+        r1, r2 = self(x1, x2)
+        self.log("val_mrr", self.mrr(r1, r2), prog_bar=True)
+        self.log("val_loss", self.loss_fn(r1, r2), prog_bar=True)
+
+    def validation_epoch_end(self, outs):
+        self.log('m_val_mrr', self.mrr.compute())
+
+    def test_step(self, batch, batch_idx):
+        id, x1, x2 = batch["id"], batch["x1"], batch["x2"]
+        r1, r2 = self(x1, x2)
+        self.write_prediction_dict({
+            "id": id,
+            "r1": r1,
+            "r2": r2
+        }, self.hparams.predictions.path)
+        self.log('test_mrr', self.mrr(r1, r2), prog_bar=True)
+
+    def test_epoch_end(self, outs):
+        self.log('m_test_mrr', self.mrr.compute())
+
+    def get_x1_encoder(self):
+        return self.x1_encoder
+
+    def get_x2_encoder(self):
+        return self.x1_encoder
+
+    @property
+    def num_training_steps(self) -> int:
+        """Total training steps inferred from datamodule and number of epochs."""
+        steps_per_epochs = len(self.train_dataloader()) / self.trainer.accumulate_grad_batches
+        max_epochs = self.trainer.max_epochs
+        return steps_per_epochs * max_epochs
---
+++ source/xSingleFormer.py
@@ -0,0 +1,203 @@
+import json
+from statistics import mean
+
+import hydra
+import nmslib
+import numpy as np
+import os
+import torch
+from omegaconf import OmegaConf
+from pytorch_lightning import Trainer
+from pytorch_lightning import loggers as pl_loggers
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
+from transformers import AutoTokenizer
+
+from source.DataModule.CodeDescDataModule import CodeDescDataModule
+from source.helper.EvalHelper import EvalHelper
+from source.helper.ExpHelper import get_sample
+from source.model.JointEncoder import JointEncoder
+from source.model.SingleEncoder import SingleEncoder
+
+
+def get_logger(hparams):
+    return pl_loggers.TensorBoardLogger(
+        save_dir=hparams.log.dir,
+        name=f"{hparams.model.name}_{hparams.data.name}_exp"
+    )
+
+
+def get_model_checkpoint_callback(hparams):
+    return ModelCheckpoint(
+        monitor="val_mrr",
+        dirpath=hparams.model_checkpoint.dir,
+        filename=hparams.model.name + "_" + hparams.data.name,
+        save_top_k=1,
+        save_weights_only=True,
+        mode="max"
+    )
+
+
+def get_early_stopping_callback(hparams):
+    return EarlyStopping(
+        monitor='val_mrr',
+        patience=hparams.patience,
+        min_delta=hparams.min_delta,
+        mode='max'
+    )
+
+
+def get_tokenizer(hparams):
+    tokenizer = AutoTokenizer.from_pretrained(
+        hparams.tokenizer.architecture
+    )
+    if hparams.tokenizer.architecture == "gpt2":
+        tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+    return tokenizer
+
+
+def fit(hparams):
+    print("Fitting with the following parameters:\n", OmegaConf.to_yaml(hparams))
+
+    # logger
+    tb_logger = get_logger(hparams)
+
+    # checkpoint callback
+    checkpoint_callback = get_model_checkpoint_callback(hparams)
+
+    # early stopping callback
+    early_stopping_callback = get_early_stopping_callback(hparams.trainer)
+
+    # tokenizers
+    x1_tokenizer = get_tokenizer(hparams.model)
+    x2_tokenizer = x1_tokenizer
+
+    dm = CodeDescDataModule(hparams.data, x1_tokenizer, x2_tokenizer)
+
+    model = SingleEncoder(hparams.model)
+
+    trainer = Trainer(
+        fast_dev_run=hparams.trainer.fast_dev_run,
+        max_epochs=hparams.trainer.max_epochs,
+        gpus=1,
+        logger=tb_logger,
+        callbacks=[checkpoint_callback, early_stopping_callback]
+    )
+
+    # training
+    dm.setup('fit')
+    trainer.fit(model, datamodule=dm)
+
+    # testing
+    dm.setup('test')
+    trainer.test(model, datamodule=dm)
+
+
+def predict(hparams):
+    print("Predicting with the following parameters:\n", OmegaConf.to_yaml(hparams))
+
+    # tokenizers
+    x1_tokenizer = get_tokenizer(hparams.model)
+    x2_tokenizer = x1_tokenizer
+
+    # data
+    dm = CodeDescDataModule(hparams.data, x1_tokenizer, x2_tokenizer)
+
+    # model
+    model = JointEncoder.load_from_checkpoint(
+        checkpoint_path=hparams.model_checkpoint.dir + hparams.model.name + "_" + hparams.data.name + ".ckpt"
+    )
+
+    # trainer
+    trainer = Trainer(
+        fast_dev_run=hparams.trainer.fast_dev_run,
+        max_epochs=hparams.trainer.max_epochs,
+        enable_pl_optimizer=True,
+        gpus=1
+    )
+
+    # testing
+    dm.setup('test')
+    trainer.test(model=model, datamodule=dm)
+
+
+def eval(hparams):
+    print("Evaluating with the following parameters:\n", OmegaConf.to_yaml(hparams))
+    evaluator = EvalHelper(hparams)
+    evaluator.perform_eval()
+
+
+def explain(hparams):
+    print("using the following parameters:\n", OmegaConf.to_yaml(hparams))
+    # override some of the params with new values
+    model = JointEncoder.load_from_checkpoint(
+        checkpoint_path=hparams.model_checkpoint.dir + hparams.model.name + "_" + hparams.data.name + ".ckpt",
+        **hparams.model
+    )
+
+    # tokenizers
+    x1_tokenizer = get_tokenizer(hparams.model)
+    x2_tokenizer = x1_tokenizer
+
+    x1_length = hparams.data.x1_length
+    x2_length = hparams.data.x2_length
+
+    desc, code = get_sample(
+        hparams.attentions.sample_id,
+        hparams.attentions.dir + hparams.data.name + "_samples.jsonl"
+    )
+
+    x1 = x1_tokenizer.encode(text=desc, max_length=x1_length, padding="max_length",
+                             truncation=True)
+    x2 = x2_tokenizer.encode(text=code, max_length=x2_length, padding="max_length",
+                             truncation=True)
+
+    # predict
+    model.eval()
+
+    r1_attentions, r2_attentions = model(torch.tensor([x1]), torch.tensor([x2]))
+
+    attentions = {
+        "x1": desc,
+        "x1_tokens": x1_tokenizer.convert_ids_to_tokens(x1),
+        "x2": code,
+        "x2_tokens": x2_tokenizer.convert_ids_to_tokens(x2),
+        "r1_attentions": r1_attentions,
+        "r2_attentions": r2_attentions
+    }
+    torch.save(obj=attentions,
+               f=hparams.attentions.dir +
+                 hparams.model.name +
+                 "_" +
+                 hparams.data.name +
+                 "_attentions.pt")
+
+
+@hydra.main(config_path="configs/", config_name="config.yaml")
+def perform_tasks(hparams):
+    os.chdir(hydra.utils.get_original_cwd())
+    hparams = update_hparams(hparams)
+
+    if "fit" in hparams.tasks:
+        fit(hparams)
+    if "predict" in hparams.tasks:
+        predict(hparams)
+    if "eval" in hparams.tasks:
+        eval(hparams)
+    if "explain" in hparams.tasks:
+        explain(hparams)
+
+
+def update_hparams(hparams):
+    # update predictions
+    hparams.model.predictions.path = f"../resources/predictions/{hparams.model.name}_{hparams.data.name}_predictions.pt"
+
+    # update interpolation util the nesse update
+    if hparams.model.name == "cnn":
+        hparams.model.x1_encoder_hparams.sentence_length = hparams.data.x1_length
+        hparams.model.x2_encoder_hparams.sentence_length = hparams.data.x2_length
+
+    return hparams
+
+
+if __name__ == '__main__':
+    perform_tasks()
