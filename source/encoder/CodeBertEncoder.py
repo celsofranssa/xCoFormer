9f2a7a384b65b4cfb2c9e2185a8093bcd19a737d
celso <celsofranca@dcc.ufmg.br>
2021-02-01 15:33:52 -0300

    refactor: adjust codebert.yaml.

---
+++ source/encoder/CodeBertEncoder.py
@@ -0,0 +1,17 @@
+from pytorch_lightning import LightningModule
+from transformers import RobertaModel
+
+class CodeBertEncoder(LightningModule):
+    """Encodes the input as embeddings."""
+
+    def __init__(self, hparams):
+        super(CodeBertEncoder, self).__init__()
+        self.codeberta_encoder = RobertaModel.from_pretrained(
+            hparams.architecture,
+            output_attentions=hparams.output_attentions
+        )
+
+    def forward(self, features):
+
+        attention_mask = (features != 1).int()
+        return self.codeberta_encoder(features, attention_mask).pooler_output
