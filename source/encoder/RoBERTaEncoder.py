8cc88befd99a81c86aab231ba536971536de0734
Celso <celsofranca@dcc.ufmg.br>
2021-05-09 13:59:54 -0300

    refactor: update RoBERTaEncoder.py.

--- source/encoder/RoBERTaEncoder.py
+++ source/encoder/RoBERTaEncoder.py
@@ -1,14 +1,31 @@
+import importlib
+
 from pytorch_lightning import LightningModule
 from transformers import RobertaModel
-
 
 class RoBERTaEncoder(LightningModule):
     """Encodes the input as embeddings."""
 
     def __init__(self, hparams):
         super(RoBERTaEncoder, self).__init__()
-        self.roberta_encoder = RobertaModel.from_pretrained(hparams.architecture)
+        self.encoder = RobertaModel.from_pretrained(
+            hparams.architecture,
+            output_attentions=hparams.output_attentions
+        )
+        self.pooling = self.get_pooling(hparams.pooling, hparams.pooling_hparams)
+
+    @staticmethod
+    def get_pooling(pooling, pooling_hparams):
+        pooling_module, pooling_class = pooling.rsplit('.', 1)
+        pooling_module = importlib.import_module(pooling_module)
+        return getattr(pooling_module, pooling_class)(pooling_hparams)
 
     def forward(self, features):
-        attention_mask = (features > 1).int()
-        return self.roberta_encoder(features, attention_mask).pooler_output
+        attention_mask = (features != 1).int()
+        encoder_outputs = self.encoder(features, attention_mask)
+
+        return self.pooling(
+            attention_mask,
+            encoder_outputs
+        )
+
