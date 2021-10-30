0c3af2bde6c89ced541110081b5ea61712509122
Celso <c.el@live.com>
2021-10-29 21:43:32 -0300

    feat: add BiLSTM settings.

--- source/encoder/BiLSTMEncoder.py
+++ source/encoder/BiLSTMEncoder.py
@@ -5,22 +5,20 @@ from pytorch_lightning import LightningModule
 from torch import nn
 
 from source.encoder.EncoderOutput import EncoderOutput
-from source.pooling.MaxPooling import MaxPooling
 
 
-class LSTMEncoder(LightningModule):
-    """Encodes the input as embedding. Url article: https://guxd.github.io/papers/deepcs.pdf"""
+class BiLSTMEncoder(LightningModule):
+    """Encodes the input as embeddings."""
 
     def __init__(self, vocabulary_size, representation_size, hidden_size, pooling):
-        """Set the network paramets."""
-        super(LSTMEncoder, self).__init__()
+        super(BiLSTMEncoder, self).__init__()
 
         self.embedding = nn.Embedding(
             num_embeddings=vocabulary_size,
             embedding_dim=representation_size
         )
 
-        self.lstm = torch.nn.LSTM(
+        self.rnn = torch.nn.LSTM(
             input_size=representation_size,
             hidden_size=hidden_size,
             batch_first=True,
@@ -30,11 +28,10 @@ class LSTMEncoder(LightningModule):
 
         self.pooling = pooling
 
-
     def forward(self, x):
-        attention_mask = (x > 0).int()
+        attention_mask = (x > 1).int()
         emb_outs = self.embedding(x)
-        last_hidden_state, pooler_output = self.lstm(emb_outs)
+        last_hidden_state, pooler_output = self.rnn(emb_outs)
 
         last_hidden_state = self.linear(last_hidden_state)
 
