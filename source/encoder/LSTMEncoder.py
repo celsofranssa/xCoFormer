ef64cd2b33f7bea503d873b87be20754a5bf7510
Celso <c.el@live.com>
2021-12-06 13:18:25 -0300

    refactor: adjust LSTM encoder.

--- source/encoder/LSTMEncoder.py
+++ source/encoder/LSTMEncoder.py
@@ -16,7 +16,7 @@ class LSTMEncoder(LightningModule):
             embedding_dim=representation_size
         )
 
-        self.rnn = torch.nn.LSTM(
+        self.lstm = torch.nn.LSTM(
             input_size=representation_size,
             hidden_size=hidden_size,
             batch_first=True,
@@ -29,7 +29,7 @@ class LSTMEncoder(LightningModule):
     def forward(self, x):
         attention_mask = (x > 1).int()
         emb_outs = self.embedding(x)
-        last_hidden_state, pooler_output = self.rnn(emb_outs)
+        last_hidden_state, pooler_output = self.lstm(emb_outs)
 
         last_hidden_state = self.linear(last_hidden_state)
 
