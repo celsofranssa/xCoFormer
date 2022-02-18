ffdef5cea1718292b1c06475b846fda35f535a9b
Celso <c.el@live.com>
2022-02-18 12:04:58 -0300

    feat: update PL version.

--- run.sh
+++ run.sh
@@ -4,12 +4,14 @@ export PYTHONPATH=$PATHONPATH:~/projects/xCoFormer/
 
 # RoCLM_CoTNG PYTHON
 python main.py \
-  tasks=[fit,predict,eval] \
-  model=RoCLM_CoTNG \
-  data=PYTHON \
+  tasks=[fit] \
+  model=FNet_CoTNG \
+  data=JAVA \
   data.folds=[0] \
   data.batch_size=64 \
   data.num_workers=64 \
   trainer.max_epochs=16 \
   trainer.patience=7 \
-  trainer.min_delta=0.03
+  trainer.min_delta=0.03 \
+  trainer.precision=32 \
+  trainer.max_steps=1000
